3dce9a62d6a5e0dcffe9a734c2d14a16d7a66ef2
Pak Andrey <scuall1992@gmail.com>
2019-10-01 12:52:29 +0300

    Добавил оптимизацию

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PIL import Image
 for i in os.listdir("."):
     if ".jpg" in i:
         img = Image.open(i)
+        img.thumbnail((100, 100))
         pixels = list(img.getdata())
         if len(set(["".join(map(str, i)) for i in pixels])) == 1:
             os.remove(i)
